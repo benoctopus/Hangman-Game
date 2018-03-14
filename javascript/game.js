3cc1ae1d265b05010fc990118b14c1b60a2f2858
benjamin rose <rose.benjamin925@gmail.com>
2018-03-14 14:46:38 -0700

    completed core game

--- javascript/game.js
+++ javascript/game.js
@@ -22,7 +22,28 @@ var gameEnv = {
         "tango"
     ],
 
-    randomWord: function() {
+    parts: [
+        function() {
+            canvas.addChild(man.head)
+        },
+        function() {
+            canvas.addChild(man.body)
+        },
+        function() {
+            canvas.addChild(man.lArm)
+        },
+        function() {
+            canvas.addChild(man.rArm)
+        },
+        function() {
+            canvas.addChild(man.lLeg)
+        },
+        function() {
+            canvas.addChild(man.rLeg)
+        }
+    ],
+
+    randomWord: function () {
         //pick random word from word bank
         var i = Math.floor(Math.random() * (gameEnv.bank.length));
         var wd = this.bank[i];
@@ -30,50 +51,81 @@ var gameEnv = {
         return wd
     },
 
-    toArray: function(string) {
+    toArray: function (string) {
         var temp = [];
-        for(var i = 0; i < string.length; i++) {
+        for (var i = 0; i < string.length; i++) {
             temp.push(string[i])
         }
         return temp;
     },
 
-    arrayToString: function(array) {
+    arrayToString: function (array) {
         var temp = "";
-        for(var i = 0; i < array.length; i++) {
+        for (var i = 0; i < array.length; i++) {
             temp += array[i]
         }
         return temp;
     },
 
-    checkWord: function(character) {
+    keyListener: function() {
+        if (this.lCount >= 6) {
+            this.losses += 1;
+            instructions.text("You Loose");
+            play_again.text("press the space bar to try again");
+            main()
+        }
+        else if (this.sCount >= this.currentW.length) {
+            this.wins += 1;
+            instructions.text("You Win");
+            play_again.text("press the space bar to play again");
+            main()
+        }
+        listener2.on("keyup", function(event) {
+            e = event.key;
+            listener2.off();
+            gameEnv.checkWord(e)
+        })
+    },
+
+    checkWord: function (character) {
+        console.log(character);
         var condition = true;
         var gotIt = false;
         this.currentW = this.toArray(this.currentW);
         this.hiddenW = this.toArray(this.hiddenW);
-        while(condition) {
+        while (condition) {
             var i = this.currentW.indexOf(character);
-            if(i > -1) {
+            if (i > -1) {
                 this.currentW[i] = "_";
                 this.hiddenW[i] = character;
                 gotIt = true;
                 this.sCount++;
             }
-            else if (i < 0 && gotIt === true ) {
+            else if (i < 0 && gotIt === true) {
                 break;
             }
-            else {
-                this.lCount++;
+            else if (i < 0 && character === " ") {
+                gotIt = true;
                 break
+            }
+            else {
+                if(this.lCount < 6) {
+                    this.parts[this.lCount]();
+                    this.lCount++;
+                    break
+                }
             }
         }
         this.currentW = this.arrayToString(this.currentW);
         this.hiddenW = this.arrayToString(this.hiddenW);
         this.word.text(this.hiddenW);
+        listener2.off();
+        this.keyListener()
     },
 
     startGame: function () {
         //setup new game environment
+        play_again.text("     ");
         this.sCount = 0;
         this.lCount = 0;
         this.currentW = gameEnv.randomWord();
@@ -82,122 +134,176 @@ var gameEnv = {
         for (var i = 0; i < gameEnv.currentW.length; i++) {
             this.hiddenW += "_";
         }
-        guillotine.canvas.reset();
+        canvas.reset();
+        canvas.addChild(man.base);
+        canvas.addChild(man.vertical);
+        canvas.addChild(man.top);
+        canvas.addChild(man.noose);
+        // canvas.addChild(man.head);
+        // canvas.addChild(man.body);
+        // canvas.addChild(man.lArm);
+        // canvas.addChild(man.lLeg);
+        // canvas.addChild(man.rArm);
+        // canvas.addChild(man.rLeg);
         instructions.text("Guess a letter");
         this.word.text(this.hiddenW);
         console.log(gameEnv.currentW);
+        this.keyListener()
     }
 };
 
-canvas = oCanvas.create({
+var canvas = oCanvas.create({
     canvas: "#canvas",
     fps: 60
 });
 
-var guillotine = {
-    //draw all body parts separately
-
-    head: function() {
-        return this.canvas.display.ellipse({
-        x: this.canvas.width / 2,
-        y: this.canvas.height * (.25),
-        radius_x: this.canvas.width / 12,
-        radius_y: this.canvas.height / 12,
-        stroke: "2px black"
-    })},
-    body: function() {
-        return this.canvas.display.line({
+var man = {
+    base: canvas.display.line({
         start: {
-            x: this.canvas.width / 2,
-            y: this.canvas.height * (.25) + this.canvas.height / 12
+            x: 0,
+            y: canvas.height
         },
         end: {
-            x: this.canvas.width/2,
-            y: this.canvas.height * .70
+            x: canvas.width/4,
+            y: canvas.height
         },
         stroke: "3px black",
         cap: "flat"
-    })},
-    LArm: function() {
-        return this.canvas.display.line({
+    }),
+    vertical: canvas.display.line({
         start: {
-            x: this.canvas.width / 2,
-            y: this.canvas.height / 2 * .80
+            x: canvas.width/8,
+            y: 0
         },
         end: {
-            x: this.canvas.width/2 - this.canvas.width/5,
-            y: this.canvas.height *.50
+            x: canvas.width/8,
+            y: canvas.height
+        },
+        stroke: "3px black",
+        cap: "flat"
+    }),
+    top: canvas.display.line({
+        start: {
+            x: canvas.width/8,
+            y: 0
+        },
+        end: {
+            x: canvas.width/2,
+            y: 0
+        },
+        stroke: "3px black",
+        cap: "flat"
+    }),
+    noose: canvas.display.line({
+        start: {
+            x: canvas.width/2,
+            y: 0
+        },
+        end: {
+            x: canvas.width/2,
+            y: canvas.height * .25 - canvas.height/12
+        },
+        stroke: "3px black",
+        cap: "flat"
+    }),
+    head: canvas.display.ellipse({
+        x: canvas.width / 2,
+        y: canvas.height * (.25),
+        radius_x: canvas.width / 12,
+        radius_y: canvas.height / 12,
+        stroke: "2px black"
+    }),
+    body: canvas.display.line({
+        start: {
+            x: canvas.width / 2,
+            y: canvas.height * (.25) + canvas.height / 12
+        },
+        end: {
+            x: canvas.width/2,
+            y: canvas.height * .70
+        },
+        stroke: "3px black",
+        cap: "flat"
+    }),
+    lArm: canvas.display.line({
+        start: {
+            x: canvas.width / 2,
+            y: canvas.height / 2 * .80
+        },
+        end: {
+            x: canvas.width/2 - canvas.width/5,
+            y: canvas.height *.50
         },
         cap: "flat",
         stroke: "2px black"
-    })},
-    RArm: function() {
-        return this.canvas.display.line({
+    }),
+    rArm: canvas.display.line({
         start: {
-            x: this.canvas.width / 2,
-            y: this.canvas.height / 2 * .80
+            x: canvas.width / 2,
+            y: canvas.height / 2 * .80
         },
         end: {
-            x: this.canvas.width/2 + this.canvas.width/5,
-            y: this.canvas.height *.50
+            x: canvas.width/2 + canvas.width/5,
+            y: canvas.height *.50
         },
         cap: "flat",
         stroke: "2px black"
-    })},
-    LLeg: function() {
-        return this.canvas.display.line({
+    }),
+    lLeg: canvas.display.line({
         start: {
-            x: this.canvas.width/2,
-            y: this.canvas.height * .70
+            x: canvas.width/2,
+            y: canvas.height * .70
         },
         end: {
-            x: this.canvas.width/2 * .75,
-            y: this.canvas.height * .95
+            x: canvas.width/2 * .75,
+            y: canvas.height * .95
         },
         cap: "flat",
         stroke: "3px black"
-    })},
-    RLeg: function() {
-        return this.canvas.display.line({
+    }),
+    rLeg: canvas.display.line({
         start: {
-            x: this.canvas.width/2,
-            y: this.canvas.height * .70
+            x: canvas.width/2,
+            y: canvas.height * .70
         },
         end: {
-            x: this.canvas.width/2 * 1.25,
-            y: this.canvas.height * .95
+            x: canvas.width/2 * 1.25,
+            y: canvas.height * .95
         },
         cap: "flat",
         stroke: "3px black"
-    })},
-    drawList: [
-        this.head,
-        this.body,
-        this.LArm,
-        this.RArm,
-        this.LLeg,
-        this.RLeg
-    ],
-    drawNext: function (count) {
-        this.canvas.addChild(this.drawList[count]);
-    }
+    })
 };
 //declare jQuery objects
-
 var instructions = $("#instructions");
+var play_again = $("#replay");
+play_again.text("   ");
 var listener1 = $(document);
+var listener2 = $(document);
 //start the magic
 listener1.ready(main());
 
 function main() {
     if(gameEnv.active === false) {
-        instructions.text("Press any key to begin");
+        instructions.text("Press space bar to begin");
     }
-    listener1.on("keypress", function(){
-        gameEnv.startGame();
-        listener1.off()
+    listener1.on("keypress", function(event){
+        var e = event.key;
+        if (e === " ") {
+            console.log(e);
+            listener1.off();
+            gameEnv.startGame();
+        }
     });
 }
+
+// function keyListener() {
+//         listener2.on("keyup", function(event) {
+//             e = event.key;
+//             listener2.off();
+//             gameEnv.checkWord(e)
+//         });
+// }
 
 
 // canvas.addChild(man.head);
